e48c863f09ea17310570c14148e32987c2b6627e
libiunc <libriunc@gmail.com>
2024-11-02 04:15:44 +0800

    Add support for licheepi4a (standardization), but pacth still needs to be modified
    Signed-off-by: libiunc <libriunc@gmail.com>

--- target/linux/xuantie/th1520/target.mk
+++ target/linux/xuantie/th1520/target.mk
@@ -1,5 +1,16 @@
-include $(TOPDIR)/rules.mk
+BOARDNAME:=th1520
 
-BOARDNAME := Generic TH1520
-DTSDIR := thead
-DTSNAMES := th1520-lichee-pi-4a th1520-lichee-pi-4a-16g
+#include $(TOPDIR)/rules.mk
+
+
+##ARCH:=aarch64
+##SUBTARGET:=th1520
+
+##BOARDNAME := Sipeed Lichee Pi 4A 16g
+##DEFAULT_PACKAGES += fitblk uboot-envtools
+##DTSDIR := thead
+##DTSNAMES := th1520-lichee-pi-4a th1520-lichee-pi-4a-16g
+
+##BOARDNAME:=Sipeed Lichee Pi 4A 8g
+##DTSDIR:=thead
+##DTSNAME:=light-lpi4a
